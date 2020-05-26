6df7ecbab99f9bceafc255437608c2209144a8a2
Olivia Li <oliviaa.li@mail.utoronto.ca>
2020-05-26 00:36:19 -0400

    using google places api cuz faster

--- tourist.py
+++ tourist.py
@@ -1,47 +1,120 @@
 import json
 import sys
 from datetime import datetime
-from geopy.geocoders import Nominatim
+import requests
+
+YOUR_API_KEY =
 
 
-with open('history.json') as json_file:
-    data = json.load(json_file)
-
-def getDataAfter(timeStamp):
+def getPastYearFiles(date):
     lst = []
-    for entry in data:
-        if int(entry["timestampMs"]) >= timeStamp:
-            lst.append(entry)
+    date_object = datetime.strptime(date, '%m-%d-%Y').date()
+    months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+    for month in months[:date_object.month]:
+        lst.append(f'{date_object.year}/{date_object.year}_{month.upper()}.json')
+    if len(lst) < 12:
+        for month in months[date_object.month:]:
+            lst.append(f'{date_object.year-1}/{date_object.year-1}_{month.upper()}.json')
     return lst
 
-def getDayData(lst):
-    temp = int(lst[0]["timestampMs"])//1000
+def getCleanedData(file_names):
+    lst = []
+    for date in file_names:
+        with open(date) as json_file:
+            location_object = json.load(json_file)["timelineObjects"]
+            for entry in location_object:
+                if "placeVisit" in entry:
+                    keys = ["location", "duration"]
+                    entry['placeVisit'] = {key: entry['placeVisit'][key] for key in keys}
+                    lst.append(entry)
+    return lst
+
+def removeHourlyData(entries):
+    temp = int(entries[0]["placeVisit"]["duration"]["startTimestampMs"]) // 1000
     previous_date = datetime.fromtimestamp(temp)
     cleaned_list = []
-    for item in lst[1:]:
-        timestamp = item["timestampMs"]
+    for item in entries[1:]:
+        timestamp = item["placeVisit"]["duration"]["startTimestampMs"]
         timestamp = int(timestamp) // 1000
         date = datetime.fromtimestamp(timestamp)
-        if date.day != previous_date.day or date.month != previous_date.month or date.year != previous_date.year:
+        if date.hour != previous_date.hour and date.day != previous_date.day or date.month != previous_date.month or date.year != previous_date.year:
             cleaned_list.append(item)
             previous_date = date
     return cleaned_list
 
-def entrysInAmerica(lst):
-    return_list = []
-    for entry in lst:
-        latitude = int(entry["latitudeE7"])/10**7
-        longitude = int(entry["longitudeE7"])/10**7
-        geolocator = Nominatim(user_agent="My Tourist Days")
-        location = geolocator.reverse(f'{latitude}, {longitude}')
-        location_country = location.raw["address"]["country_code"]
-        if location_country.lower() == "us":
-            return_list.append(entry)
-    return return_list
 
-if __name__ == "__main__":
-    a = getDataAfter(sys.argv[1])
-    b = entrysInAmerica(a)
-    c = getDayData(b)
-    print(f'Days spend in the US: {len(c)}')
-    print(f'Tourist Days left in the US: {182 - len(c)}')
+def getCountry(place_id, country_code):
+    url = f'https://maps.googleapis.com/maps/api/place/details/json?key={YOUR_API_KEY}&place_id={place_id}'
+    print(url)
+    response = requests.get(url)
+    res = json.loads(response.text)
+    if res["status"] == "OK":
+        for entry in res['result']['address_components']:
+            if entry["short_name"].lower() == country_code:
+                return True
+        return False
+
+
+def entrysInAmerica(entries):
+    lst = []
+    for entry in entries:
+        place_id = entry["placeVisit"]["location"]["placeId"]
+        if getCountry(place_id, "us"):
+            lst.append(entry)
+    return lst
+
+
+
+a = getPastYearFiles("05-20-2020")
+b= getCleanedData(a)
+print(len(b))
+print(b)
+c = removeHourlyData(b)
+print(len(c))
+d = entrysInAmerica(b)
+print(len(d))
+
+
+
+
+
+
+
+
+#
+# def getYearData(date):
+#     lst = []
+#     for entry in data_dictionary:
+#         if int(entry["timestampMs"]) >= int(timeStamp):
+#             lst.append(entry)
+#     return lst
+#
+# def getDayData(lst):
+    # temp = int(lst["timestampMs"])//1000
+    # previous_date = datetime.fromtimestamp(temp)
+    # cleaned_list = []
+    # for item in lst[1:]:
+    #     timestamp = item["timestampMs"]
+    #     timestamp = int(timestamp) // 1000
+    #     date = datetime.fromtimestamp(timestamp)
+    #     print(date)
+    #     if date.day != previous_date.day or date.month != previous_date.month or date.year != previous_date.year:
+    #         cleaned_list.append(item)
+    #         previous_date = date
+    # return cleaned_list
+#
+
+#
+# # if __name__ == "__main__":
+# #     a = getDataAfter(sys.argv[0])
+# #     b = entrysInAmerica(a)
+# #     c = getDayData(b)
+# #     print(f'Days spend in the US: {len(c)}')
+# #     print(f'Tourist Days left in the US: {182 - len(c)}')
+#
+# a = getDataAfter(1588710148056)
+# b = entrysInAmerica(a)
+# c = getDayData(b)
+# print(f'Days spend in the US: {len(c)}')
+# print(f'Tourist Days left in the US: {182 - len(c)}')
+
