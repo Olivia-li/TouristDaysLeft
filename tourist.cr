a18ebf29c505ada64ada3974544136b28825e9f2
Olivia Li <oliviali.ouyang@gmail.com>
2021-04-11 11:36:39 -0700

    working days counter

--- tourist.cr
+++ tourist.cr
@@ -1,9 +1,9 @@
 require "./.env.cr"
 require "json"
 require "http/client"
-require "time"
 
-file_path = "2021/2021_APRIL.json"
+directory_years = ["./2020", "./2019"]
+test_dir = ["./2021"]
 
 def clean_data(file_path)
   cleaned_locations = [] of JSON::Any
@@ -18,7 +18,7 @@ def clean_data(file_path)
 end
 
 def placeid_to_country(place_id)
-  response = HTTP::Client.get "https://maps.googleapis.com/maps/api/place/details/json?key=#{API_KEY}&place_id=#{place_id}"
+  response = HTTP::Client.get "https://maps.googleapis.com/maps/api/place/details/json?key=#{API_KEY}&place_id=#{place_id}&fields=address_component"
   json = JSON.parse(response.body.to_s)["result"]["address_components"]
   state = ""
   country = ""
@@ -33,20 +33,27 @@ def placeid_to_country(place_id)
   return [state, country]
 end
 
-def get_num_days(file_path)
+def get_days_in_file(file_path)
   count = 0
   locations = clean_data(file_path)
   current_day = 0
-  already_in_states = false
   locations.each do |place|
-    if Time.unix_ms(place[1].to_i64).day != current_day && !already_in_states
+    if Time.unix_ms(place[1].to_i64).day != current_day
+      puts Time.unix_ms(place[1].to_i64).day
+      current_day = Time.unix_ms(place[1].to_i64).day
       if placeid_to_country(place[0])[1] == "United States"
         count += 1
-        already_in_states = true
-      else
-        already_in_states = false
       end
     end
+  end
+  return count
+end
+
+def get_days_in_year(dir_path)
+  dir = Dir.open(dir_path)
+  count = 0
+  dir.each_child do |month|
+    count += get_days_in_file("#{dir_path}/#{month.to_s}")
   end
   return count
 end
