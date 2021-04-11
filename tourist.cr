b71b67d3128d7733e7fd1606e1b466f8b8f10b99
Olivia Li <oliviali.ouyang@gmail.com>
2021-04-10 23:53:41 -0700

    must iterate through all long_names

--- tourist.cr
+++ tourist.cr
@@ -20,23 +20,24 @@ end
 def placeid_to_country(place_id)
   response = HTTP::Client.get "https://maps.googleapis.com/maps/api/place/details/json?key=#{API_KEY}&place_id=#{place_id}"
   json = JSON.parse(response.body.to_s)["result"]["address_components"]
+  puts [json[5]["long_name"], json[6]["long_name"]]
   return [json[5]["long_name"], json[6]["long_name"]]
 end
 
-def get_day(ms)
-  return Time.unix_ms(ms)
-end
-
 def get_num_days(file_path)
+  count = 0
   locations = clean_data(file_path)
   current_day = 0
-  skip_day = false
+  already_in_states = false
   locations.each do |place|
-    if Time.unix_ms(place[1].to_i64).day == current_day && skip_day
-      next
-    elsif puts Time.unix_ms(place[1].to_i64).day
-      current_day = Time.unix_ms(place[1].to_i64).day
+    if Time.unix_ms(place[1].to_i64).day != current_day || !already_in_states
+      if placeid_to_country(place[0])[1] == "United States"
+        count += 1
+        already_in_states = true
+      else
+        already_in_states = false
+      end
     end
-    puts Time.unix_ms(place[1].to_i64).day
   end
+  return count
 end
