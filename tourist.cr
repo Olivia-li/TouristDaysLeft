0dbd2541655554b9fc37b9e8820b06962c72194a
Olivia Li <oliviali.ouyang@gmail.com>
2021-04-11 00:37:36 -0700

    get_num_days not currently working with if conditional logic

--- tourist.cr
+++ tourist.cr
@@ -20,8 +20,17 @@ end
 def placeid_to_country(place_id)
   response = HTTP::Client.get "https://maps.googleapis.com/maps/api/place/details/json?key=#{API_KEY}&place_id=#{place_id}"
   json = JSON.parse(response.body.to_s)["result"]["address_components"]
-  puts [json[5]["long_name"], json[6]["long_name"]]
-  return [json[5]["long_name"], json[6]["long_name"]]
+  state = ""
+  country = ""
+  json.as_a.each do |area|
+    if area["types"][0] == "administrative_area_level_1"
+      state = area["long_name"]
+    elsif area["types"][0] == "country"
+      country = area["long_name"]
+    end
+  end
+  puts [state, country]
+  return [state, country]
 end
 
 def get_num_days(file_path)
@@ -30,7 +39,7 @@ def get_num_days(file_path)
   current_day = 0
   already_in_states = false
   locations.each do |place|
-    if Time.unix_ms(place[1].to_i64).day != current_day || !already_in_states
+    if Time.unix_ms(place[1].to_i64).day != current_day && !already_in_states
       if placeid_to_country(place[0])[1] == "United States"
         count += 1
         already_in_states = true
